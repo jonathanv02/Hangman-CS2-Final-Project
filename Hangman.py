05bfc10d67eb16b0b0790306143262a06157e357
jonathanv02 <49208746+jonathanv02@users.noreply.github.com>
2019-04-18 12:47:26 -0700

    Progress

--- Hangman.py
+++ Hangman.py
@@ -23,12 +23,17 @@ def draw_window():
     win.yUp()	#makes (0,0) at the bottom left
     return win
 
-#def draw_circle(win):
+def draw_circle(center, radius, color, win):
+    circle = Circle(center, radius)
+    circle.draw(win)
 
-#def draw_line(win):
+def draw_line(P1, P2, win):
+    line = Line(P1, P2)
+    line.draw(win)
 
-#def draw_rect():
-
+def draw_rect(x1, y1, x2, y2, win):
+    rect = Rectangle(Point(x1, y1), Point(x2, y2))
+    rect.draw(win)
 
 
 def open_file():
@@ -43,33 +48,46 @@ def random_word(file_list):
     return random_word
 
 
+def draw_structure(win):
+    w = win.getwidth()
+    h = win.getHeight()
+    
+
+
+
 def hangman_game(word):
     turns = 6
     guesses = ""
-    while turns > 0:
+    word_len = len(word)-1
+    print " The word is {} letters long".format(word_len)
+    while len(guesses) != len(word):
         guess = raw_input("Guess a letter:")                  
         if guess not in word:
             turns -= 1
-            print " Your have {} turns left".format(turns)
+            print "Your have {} turns left".format(turns)
         else:
             guesses += guess
+            ch_loc = word[guess]
+            print "You guessed a letter in letter {} of the word".format(ch_loc)
                 
-            
-    if turns == 0:
+    print guesses
+    print len(guesses)
+    print len(word)
+    if len(guesses) == len(word):
+       print " you win with {} turns left".format(turns)
+    elif turns == 0:
         print "You Lose"
-    else:
-        print " you win with {] turns left".format(turns) 
-    print "{}".format(word)
-
+        print "{}".format(word)
 
 
 def main():
     #win = draw_window()
     file_list = open_file()
-    word = random_word(file_list)
+    word = "work"
     hangman_game(word)
     #win.getMouse()  #waits for the user to click the screen
     #win.close()	    #closes the window
        
 if __name__ == "__main__":
     main()
+    
